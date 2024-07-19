3ff91f60c3790c342423f6c42259ef4276b64d0d
MarcoParucho <125199963+MarcoParucho@users.noreply.github.com>
2024-07-19 15:22:26 -0400

    Broken Master Piece
    Added react leaflet and while the buttons are missing in the map. and a lot of things are not working! I can see the sensors data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { loadModules } from 'esri-loader';
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'esri-leaflet';
 import IconButton from '@mui/material/IconButton';
-import ToggleOnIcon from '@mui/icons-material/ToggleOn';
-import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 import NavigationIcon from '@mui/icons-material/Navigation';
 
-// Define the API URL
+
 const config = {
     API_URL: 'http://wahoobay-dev-env-v3.eba-pb7w9arq.us-east-2.elasticbeanstalk.com',
 };
 
 const Map = () => {
-    const mapRef = useRef(null);
-    const [basemap, setBasemap] = useState('osm');
     const [sensor1Info, setSensor1Info] = useState(null);
-    const [currentMarker, setCurrentMarker] = useState(0); // State to keep track of the current marker
-    const viewRef = useRef(null); // Ref to store the MapView instance
+    const [currentMarker, setCurrentMarker] = useState(0);
 
     useEffect(() => {
-        fetchData(`${config.API_URL}/api/v1/sitedata/latest?site=1`) // site url 1
+        fetchData(`${config.API_URL}/api/v1/sitedata/latest?site=1`)
             .then((data) => {
-                // Process data for site 1
                 console.log('Data for site 1:', data);
-                setSensor1Info(data?.readings); // Set the sensor info state
+                setSensor1Info(data?.readings);
             })
             .catch((error) => {
                 console.error('Error fetching data for site 1:', error);
             });
     }, []);
 
-    // Function to handle basemap change
-    const handleBasemapChange = () => {
-        setBasemap(basemap === 'osm' ? 'satellite' : 'osm');
-    };
-
-    // Function to fetch sensor data
     const fetchData = async (url) => {
         try {
             const response = await fetch(url);
@@ -49,161 +40,76 @@ const Map = () => {
         }
     };
 
-    useEffect(() => {
-        loadModules(
-            ['esri/Map', 'esri/views/MapView', 'esri/layers/GraphicsLayer', 'esri/Graphic', 'esri/PopupTemplate'],
-            { css: true }
-        )
-            .then(([Map, MapView, GraphicsLayer, Graphic, PopupTemplate]) => {
-                // Create a new map
-                const map = new Map({
-                    basemap: basemap,
-                });
+    const markerLocations = [
+        {
+            longitude: -80.08335,
+            latitude: 26.2603,
+            title: 'Wahoo Bay',
+            color: 'red',
+        },
+        {
+            longitude: -80.0865,
+            latitude: 26.235,
+            title: 'Live Feed',
+            color: 'purple',
+            video: '/WahooBay_video.mp4', // Ensure this path is correct
+        },
+        {
+            longitude: -80.0873,
+            latitude: 26.2351,
+            title: 'Pompano Fishing Pier',
+            color: 'yellow',
+        },
+    ];
 
-                // Create a new map view
-                const view = new MapView({
-                    container: mapRef.current,
-                    map: map,
-                    center: [-80.0828, 26.25955], // Initial center of the map
-                    zoom: 16,
-                    constraints: {
-                        minZoom: 16,
-                        maxZoom: 18,
-                        rotationEnabled: false, // Disable rotation
-                        snapToZoom: false, // Disable zoom snapping
-                    },
-                    navigation: {
-                        mouseWheelZoomEnabled: false, // Disable zooming with mouse wheel
-                        browserTouchPanEnabled: false, // Disable panning with touch gestures
-                    },
-                });
-
-                // Store the view instance in the ref
-                viewRef.current = view;
-
-                // Disable map interaction
-                view.on("drag", (event) => {
-                    event.stopPropagation();
-                });
-
-                // Create a graphics layer to hold the markers
-                const graphicsLayer = new GraphicsLayer();
-
-                // Add the graphics layer to the map
-                map.add(graphicsLayer);
-
-                // Array of marker locations
-                const markerLocations = [
-                    {
-                        longitude: -80.08335,
-                        latitude: 26.2603,
-                        title: 'Wahoo Bay',
-                        content: 'https://beta.wahoobay.net/pages/dashpage',
-                        color: 'red',
-                    },
-                    {
-                        longitude: -80.0865,
-                        latitude: 26.2350,
-                        title: 'Pompano Fishing Pier',
-                        content: 'https://example.com/second',
-                        color: 'yellow',
-                    },
-                ];
-
-                // Create and add marker graphics for each location
-                markerLocations.forEach((location) => {
-                    // Create a marker graphic
-                    const markerGraphic = new Graphic({
-                        geometry: {
-                            type: 'point',
-                            longitude: location.longitude,
-                            latitude: location.latitude,
-                        },
-                        symbol: {
-                            type: 'simple-marker',
-                            style: 'diamond',
-                            color: location.color,
-                            size: '17px',
-                            outline: {
-                                color: [255, 255, 255],
-                                width: 1,
-                            },
-                        },
-                    });
-
-                    // Create a popup template
-                    const popupTemplate = new PopupTemplate({
-                        title: location.title,
-                        content: `<div >
-                                    Air Temperature: ${sensor1Info ? sensor1Info[7] : 'Loading...'}° C<br>
-                                    Water Level: ${sensor1Info ? sensor1Info[18] : 'Loading...'} meters<br>
-                                    Average Wind Speed today: ${sensor1Info ? sensor1Info[5] : 'Loading...'}<br>
-                                    <a style={{marginTop: "1rem"}} href="${location.content}" target="_blank">View Dashboard</a>
-                                  </div>`,
-                    });
-
-                    // Set the popup template for the marker graphic
-                    markerGraphic.popupTemplate = popupTemplate;
-
-                    // Add the marker graphic to the graphics layer
-                    graphicsLayer.add(markerGraphic);
-                });
-
-                // Fetch sensor data
-                fetchData(`${config.API_URL}/api/v1/sitedata/latest?site=1`) // site url 1
-                    .then((data) => {
-                        // Process data for site 1
-                        console.log('Data for site 1:', data);
-                        setSensor1Info(data); // Set the sensor info state
-                    })
-                    .catch((error) => {
-                        console.error('Error fetching data for site 1:', error);
-                    });
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }, [basemap]);
-
-    // Function to switch to the next marker
     const switchMarker = () => {
-        const markerLocations = [
-            {
-                longitude: -80.08355,
-                latitude: 26.2606,
-                title: 'Wahoo Bay',
-                content: 'https://beta.wahoobay.net/pages/dashpage',
-                color: 'red',
-            },
-            {
-                longitude: -80.0890,
-                latitude: 26.2354,
-                title: 'Second Marker',
-                content: 'https://example.com/second',
-                color: 'red',
-            },
-        ];
-
         const nextMarker = (currentMarker + 1) % markerLocations.length;
         setCurrentMarker(nextMarker);
-        if (viewRef.current) {
-            viewRef.current.center = [markerLocations[nextMarker].longitude, markerLocations[nextMarker].latitude];
-        }
     };
 
+    const esriBasemapUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer';
+
     return (
-        <div style={{ marginTop: '9rem', height: '80vh', width: '80%', position: 'relative' }}>
-            <div ref={mapRef} style={{ height: 'calc(100% - 48px)', marginBottom: '20px' }} />
+        <div style={{ marginTop: '9rem', height: '80vh', width: '80%' }}>
+            <MapContainer center={[26.25955, -80.0828]} zoom={16} style={{ height: '100%', width: '100%' }}>
+                <TileLayer
+                    url={`${esriBasemapUrl}/tile/{z}/{y}/{x}`}
+                />
+                {markerLocations.map((location, index) => (
+                    <Marker
+                        key={index}
+                        position={[location.latitude, location.longitude]}
+                        icon={L.icon({
+                            iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${location.color}.png`,
+                            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                            iconSize: [25, 41],
+                            iconAnchor: [12, 41],
+                            popupAnchor: [1, -34],
+                            shadowSize: [41, 41]
+                        })}
+                    >
+                        <Popup>
+                            {location.color === 'purple' ? (
+                                <div>
+                                    <video controls width="100%">
+                                        <source src={location.video} type="video/mp4" />
+                                        Your browser does not support the video tag.
+                                    </video>
+                                </div>
+                            ) : (
+                                <div>
+                                    <div>{location.title}</div>
+                                    <div>Air Temperature: {sensor1Info ? sensor1Info[7] : 'Loading...'}° C</div>
+                                    <div>Water Level: {sensor1Info ? sensor1Info[18] : 'Loading...'} meters</div>
+                                    <div>Average Wind Speed today: {sensor1Info ? sensor1Info[5] : 'Loading...'} km/h</div>
+                                    <a href="https://beta.wahoobay.net/pages/dashpage" target="_blank" rel="noopener noreferrer">View Dashboard</a>
+                                </div>
+                            )}
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
             <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
-                {/* Button with switch icon to toggle between basemaps */}
-                <IconButton onClick={handleBasemapChange}>
-                    {basemap === 'osm' ? (
-                        <ToggleOnIcon style={{ color: 'white' }} />
-                    ) : (
-                        <ToggleOffIcon style={{ color: 'white' }} />
-                    )}
-                </IconButton>
-                {/* Button to switch between markers */}
                 <IconButton onClick={switchMarker}>
                     <NavigationIcon style={{ color: 'white' }} />
                 </IconButton>
